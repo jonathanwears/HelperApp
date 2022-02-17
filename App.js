73e69b6a8e8c776f3470d6fd6310f07c1cf82fdf
Jonathan <jonathan@pop-os.localdomain>
2022-02-17 19:05:43 +0100

    added scrolling

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, ScrollView } from 'react-native';
 import Header from './components/Header';
 import CountersArea from './components/CountersArea';
 import Udometer from './components/Udometer/Udometer';
@@ -7,12 +7,14 @@ import Udometer from './components/Udometer/Udometer';
 export default function App() {
   
   return (
-    <View style={styles.container}>
+    <ScrollView>
+       <View style={styles.container}>
       <Header />
       <StatusBar style="auto" />
       <Udometer />
       <CountersArea />
     </View>
+    </ScrollView>
   );
 }
 
@@ -21,4 +23,3 @@ const styles = StyleSheet.create({
     
   },
 });
-
