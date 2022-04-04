295a3aa5667a6d64efc6b88251cda82985862a67
Jona <jonathan@pop-os.localdomain>
2022-04-04 18:57:18 +0200

    fixing stash message

---
+++ utils/SyncCounters.js
@@ -0,0 +1,40 @@
+/* eslint-disable prefer-template */
+/* eslint-disable consistent-return */
+/* eslint-disable no-await-in-loop */
+/* eslint-disable no-restricted-syntax */
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import counterNames from './CountersList';
+
+async function syncCounters(counter) {
+  const data = await AsyncStorage.getItem(counter);
+  return data;
+}
+
+function setCounters(counts) {
+  const countersObj = counts.reduce((prevValue, currentValue) => {
+    const obj = {
+      ...prevValue,
+      [currentValue[0]]: currentValue[1],
+    };
+    return obj;
+  }, 0);
+  return countersObj;
+}
+
+async function getCounters() {
+  const counters = [];
+  for (const counter of counterNames) {
+    await syncCounters(counter).then((value) => {
+      counters.push([counter, value]);
+    });
+  }
+  return counters;
+}
+
+async function SyncCounters() {
+  const createCounters = await getCounters(counterNames);
+  const a = await setCounters(createCounters);
+  return a;
+}
+
+export default SyncCounters;
--- utils/appStore.js
+++ utils/appStore.js
@@ -1,6 +1,8 @@
+/* eslint-disable no-restricted-syntax */
 /* eslint-disable import/prefer-default-export */
 import create from 'zustand';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import SyncCounters from './SyncCounters';
 import counterNames from './CountersList';
 
 function getInitialValues() {
@@ -24,21 +26,26 @@ export const useCountersStore = create((set) => ({
   ...getInitialValues(),
 
   syncCounters: async () => {
-    const [redeployed, collected, deployed, cantFind, cantReach] = counterNames;
+    // const [redeployed, collected, deployed, cantFind, cantReach] = counterNames;
 
-    const redeployedData = await AsyncStorage.getItem(redeployed);
-    const collectedData = await AsyncStorage.getItem(collected);
-    const deployedData = await AsyncStorage.getItem(deployed);
-    const cantFindData = await AsyncStorage.getItem(cantFind);
-    const cantReachData = await AsyncStorage.getItem(cantReach);
+    // const redeployedData = await AsyncStorage.getItem(redeployed);
+    // const collectedData = await AsyncStorage.getItem(collected);
+    // const deployedData = await AsyncStorage.getItem(deployed);
+    // const cantFindData = await AsyncStorage.getItem(cantFind);
+    // const cantReachData = await AsyncStorage.getItem(cantReach);
 
+    // set({
+    //   counters: {
+    //     [redeployed]: Number(redeployedData),
+    //     [collected]: Number(collectedData),
+    //     [deployed]: Number(deployedData),
+    //     [cantFind]: Number(cantFindData),
+    //     [cantReach]: Number(cantReachData),
+    //   },
+    // });
     set({
       counters: {
-        [redeployed]: Number(redeployedData),
-        [collected]: Number(collectedData),
-        [deployed]: Number(deployedData),
-        [cantFind]: Number(cantFindData),
-        [cantReach]: Number(cantReachData),
+        ...SyncCounters(getInitialValues),
       },
     });
   },
