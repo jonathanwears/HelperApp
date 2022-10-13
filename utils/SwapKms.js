a56d213d29c850d8a1eac22396f73807d2df58b7
Jona <jonathanwears@outlook.com>
2022-10-13 10:48:14 +0200

    Update SwapKms.js

--- utils/SwapKms.js
+++ utils/SwapKms.js
@@ -3,6 +3,7 @@ import useKmStore from './stores/kmStore';
 
 function SwapKms() {
   const { startKm, finishKm } = useKmStore.getState().kilometers;
+
   // unstable_batchedUpdates required by zustand to do outside component actions;
   unstable_batchedUpdates(() => {
     useKmStore.getState().updateKm('finishKm', startKm);
